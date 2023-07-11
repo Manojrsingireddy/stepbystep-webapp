a96cee0941abf241e5354578bd555d793f4de45c
manojrsingireddy <manojsingireddy@utexas.edu>
2023-07-10 22:18:36 -0500

    added user form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import NotFound from './components/notFound/NotFound';
+import Login from './components/login/Login';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home/>} ></Route>
+            <Route path="/login" element={<Login/>} ></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/api/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// add headers for ngrock skip browser warning set to true once deploy api to ngrok
+// export default axios.create({
+//     baseURL: 'https://ea56-2600-1700-d00-c280-2469-b51f-f8d9-83d2.ngrok.io/',
+//     headers : {'ngrok-skip-browser-warning':'true'}
+// });
+
+export default axios.create({
+    baseURL: 'http://localhost:8080/'
+});
